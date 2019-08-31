7c6749d010c82c86fc12b76da13eeaf7cd473505
JuanGA12 <juan.galvez@utec.edu.pe>
2019-08-31 00:27:17 -0500

    Cambios

--- Matriz_Dispersa/Matriz.cpp
+++ Matriz_Dispersa/Matriz.cpp
@@ -9,6 +9,8 @@ Matriz::Matriz(): matriz{nullptr}, filas {0}, columnas{0} {
 
 // Crear matriz
 Matriz::Matriz(int f, int c): filas {f}, columnas{c} {
+
+
     matriz = new int*[filas];
     for (int i= 0 ; i < filas; i++)
     {
@@ -21,40 +23,98 @@ Matriz::~Matriz() {
 
 }
 
-// Llenar matriz
+// Llenar matriz (por ahora se va a llenar sin dispersion para probar si funciona)
 void Matriz::llenar(){
 
+  for ( int i = 0; i < filas; i++) {
+      for (int j = 0; j < columnas; j++) {
+          std::cout << "["<<i<<"]"<<"["<<j<<"]";
+          std::cin >> matriz[i][j];
+    }
+  }
 
 }
 
 // Imprimir matriz
 void Matriz::imprimir(){
 
-
-
-}
-
-// Sumar matrices
-void Matriz::sumar(const Matriz& Matriz1){
-
-
-}
-
-// Multiplicar matrices
-void Matriz::multiplicar(const Matriz &, const Matriz &) {
-
-
+  for ( int i = 0; i < filas; i++) {
+    for (int j = 0; j < columnas; j++) {
+      cout<<matriz[i][j]<<" ";
+    }
+    cout << endl;
+  }
 
 }
 
 // Producto Punto
-void Matriz::escalar(const Matriz &) {
+void Matriz::escalar(const Matriz& M1, int num) {
 
-
+    Matriz temp(M1);
+    for (int i = 0; i < M1.filas ; ++i) {
+        for (int j = 0; j < M1.columnas; ++j) {
+            matriz[i][j] = num*temp.matriz[i][j];
+        }
+    }
 }
 
 // Trasposición de matriz
-void Matriz::transposicion(const Matriz &) {
-
+void Matriz::transposicion(const Matriz& M1) {
+    Matriz temp(M1);
+    for (int i = 0; i < M1.filas; ++i) {
+        for (int j = 0; j < M1.columnas; ++j) {
+            matriz[i][j] = temp.matriz[j][i];
+        }
+    }
 
 }
+
+// Sobrecarga de operador +
+Matriz Matriz::operator+(const Matriz& M1){
+
+    Matriz temporal(M1);
+      for(int i = 0; i < M1.filas; i++){
+        for (int j = 0; j < M1.columnas; j++) {
+          temporal.matriz[i][j] += this->matriz[i][j];
+        }
+      }
+
+      return temporal;
+}
+
+// Sobrecarga operador *
+Matriz Matriz::operator*(const Matriz& M1){
+
+    Matriz temporal(M1);
+    for(int i = 0; i < M1.filas; i++){
+        for(int j = 0; j < this->columnas; ++j){
+            temporal.matriz[i][j] = 0;
+            for(int z=0; z < M1.columnas; ++z){
+                temporal.matriz[i][j] = temporal.matriz[i][j] + this->matriz[i][z] * this->matriz[z][j];
+              }
+            }
+          }
+    return temporal;
+}
+
+Matriz& Matriz::operator=(const Matriz & M1) {
+
+    if(this->filas !=0 && this->columnas!=0){
+        delete [] this->matriz;
+    }
+
+    this->filas = M1.filas;
+    this->columnas = M1.columnas;
+
+    this->matriz = new int*[this->filas];
+
+    for (int i = 0; i < this->filas; ++i) {
+        this->matriz[i] = new int[this->columnas];
+        for (int j = 0; j < this->columnas; ++j) {
+            this->matriz[i][j] = M1.matriz[i][j];
+        }
+
+    }
+    return *this;
+}
+
--- Matriz_Dispersa/main.cpp
+++ Matriz_Dispersa/main.cpp
@@ -6,14 +6,50 @@ using namespace std;
 
 int main() {
 
+    int filas, columnas;
+    cout<<"Ingresar # filas: ";
+    cin>>filas;
+    cout<<"Ingresar # columnas: ";
+    cin>>columnas;
+
+    Matriz M1(filas,columnas);
+    Matriz M2(filas,columnas);
+    Matriz M3(filas,columnas);
+    Matriz M4(filas,columnas);
+    Matriz M5(filas,columnas);
+    Matriz M6(filas,columnas);
+
+    M1.llenar();
+    std::cout << "Matriz 1: " << '\n';
+    M1.imprimir();
+    M2.llenar();
+    std::cout << "Matriz 2:" << '\n';
+    M2.imprimir();
+    cout <<endl;
+
+    M3 = M1 + M2; //Sobrecarga operador +
+    std::cout << "El resultado de la suma es: " << '\n';
+    M3.imprimir();
+    cout <<endl;
 
 
-    Matriz M1;
-    Matriz M2;
-    Matriz M3;
+    M4 = M1 * M2; //Sobrecarga operador *
+    std::cout << "El resultado de la multiplicación es: " << '\n';
+    M4.imprimir();
+    cout <<endl;
 
+    M5.transposicion(M1);
+    std::cout << "El resultado de la matriz traspuesta es: " << '\n';
+    M5.imprimir();
+    cout <<endl;
 
-    int M4 [3][3] = { {1, 2 ,3}, {4,5,6} };
+    M6.escalar(M1,5);
+    std::cout << "El resultado de la multiplicacion escalar es: " << '\n';
+    M6.imprimir();
+    cout <<endl;
+
+    // operador = ?
+    /*int M4 [3][3] = { {1, 2 ,3}, {4,5,6} };
     int M5 [3][3] = { {15,23,31}, {14,17,18}, {41,63,65} };
     int M6 = 20;
     int M7 [4][4] = { {1, 2,3,4}, {4,5,6,7} };
@@ -25,6 +61,6 @@ int main() {
     assert(M3.escalar(M1, M2) == M6);
     assert(M3.transposicion(M1) == M7);
 
-
+    */
     return 0;
 }
